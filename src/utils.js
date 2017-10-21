b35640f39f061477ea3c64b917cde1d2fb509a8d
Victor Igor <victor.0w3@hotmail.com>
2017-10-21 17:37:32 -0200

    :art: refactoring utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,17 @@
 const { CLITypes } = require('./adts.js')
+const { compose } = require('folktale/core/lambda');
+
+
+function toLowerCase(x) {
+    return x.toLowerCase()
+}
+
+function capitalize(word) {
+  return `${word.charAt(0).toUpperCase()}${word.substring(1)}`
+}
 
 function checkParameters(input){
-  const key = input ? `${input.charAt(0).toUpperCase()}${input.slice(1)}`: ''
+  const key = compose(capitalize, toLowerCase)(input)
   return key in CLITypes ?  CLITypes[key]()
     :      /* otherwise */  CLITypes.NotExist();
 }
