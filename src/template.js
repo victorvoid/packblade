8f1df87cff4b203219e19ce0f31022c514a6c60f
Victor Igor <victor.0w3@hotmail.com>
2017-10-21 20:03:03 -0200

    :art: refactoring to use outputFile in template

--- src/template.js
+++ src/template.js
@@ -4,9 +4,8 @@ const { dump } = require('js-yaml')
 const { fromPromised, rejected, fromNodeback } = require('folktale/concurrency/task');
 
 const fsCopy = fromPromised(fs.copy)
-const fsEnsureFile = fromPromised(fs.ensureFile)
+const fsWrite = fromPromised(fs.outputFile)
 const fsReadDir = fromNodeback(require('fs').readdir)
-const fsWriteFile = fromNodeback(require('fs').writeFile)
 
 function load(){
   return fsCopy(path.resolve(__dirname, '../template/'), 'packblade/')
@@ -16,11 +15,9 @@ function load(){
 }
 
 function createDefaultsNames(directory){
-  return fsReadDir('dotfiles')
+  return fsReadDir(directory)
     .map(filesname => dump({ filesname }))
-    .chain(yaml => fsEnsureFile('packblade/roles/common/defaults/main.yml')
-        .chain( () => fsWriteFile('packblade/roles/common/defaults/main.yml', yaml))
-    )
+    .chain(yaml => fsWrite('packblade/roles/common/defaults/main.yml', yaml))
 }
 
 module.exports = {
