1aa4c4879bfa2eaecb89ec3f54756a072844775c
Victor Igor <victor.0w3@hotmail.com>
2017-11-20 22:50:02 -0200

    removing warning to build because of the add gitsubmodule package

--- src/build.js
+++ src/build.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 const config = require('./config')
 const template = require('./template')
 const { Repository, Submodule } = require("nodegit");
-const { fromPromised, rejected, waitAll } = require('folktale/concurrency/task');
+const { fromPromised, rejected, of, waitAll } = require('folktale/concurrency/task');
 
 const init = fromPromised(Repository.init)
 const addSubmodule = fromPromised(Submodule.addSetup)
@@ -14,7 +14,11 @@ function Build(){
     .chain(roles => init('./packblade', 0)
         .chain(repository => waitAll(
           roles.map(role => {
-            return addSubmodule(repository, `https://github.com/${role}.git`, `vendor/${role.split('/')[1]}`, 0)
+            return addSubmodule(
+              repository,
+              `https://github.com/${role}.git`,
+              `vendor/${role.split('/')[1]}`, 0
+            ).orElse(error => of(error))
           })
         ))
     )
